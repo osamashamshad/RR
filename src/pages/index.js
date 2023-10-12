fbeebffa1b667c83b1fc41c9010f7499d9abbc26
Osama <osamashamshad1@gmail.com>
2023-10-12 20:27:23 +0300

    part 3 chupachups

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../../src/components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle={"Home Page"}>
-      <p>sad sad sad sad sad sad sad</p>
+      <p>I MAKE ZABAR GO VROOM VROOM</p>
+
+      <StaticImage alt="CEO OF ZABAR SITTING ON A HEAVY BIKE"
+      src='../../src/images/chupachups.jpg'
+      />
     </Layout>
   )
 }
