73e7eb9c555c98bd52888b58581634e058e46fb2
Osama <osamashamshad1@gmail.com>
2023-10-12 17:09:04 +0300

    done part 2

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import Layout from '../../src/components/layout'
+
+
+const AboutPage = () => {
+    return(
+        <Layout pageTitle="About Me">
+            <p>pack pack pack</p>
+        </Layout>
+    )
+}
+
+export const Head = () => (
+<>
+<title>About Me</title>
+<meta name = "description" content="your description"/>
+</>
+)
+
+
+export default AboutPage
+
---
+++ src/pages/hello.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Greeting from '../../src/components/greeting'
+const SayHello = () => {
+    return (
+        <div>
+            <Greeting name = "osama" />
+            <Greeting name = "sadiha" />
+            <Greeting name = "faiza" />
+        </div>
+    )
+}
+
+
+export default SayHello
